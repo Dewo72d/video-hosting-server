3acfce19a715920fc0d51004e0d51804bbe07a8b
dewo72d <dewo72d@gmail.com>
2023-07-01 20:07:19 +0300

    add test video stream

--- src/controllers/videos.controller.ts
+++ src/controllers/videos.controller.ts
@@ -1,5 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, StreamableFile, Headers, Header, Res, HttpStatus } from '@nestjs/common';
 import { VideosService } from '../services/videos.service';
+import { createReadStream, statSync } from 'fs';
+import { join } from 'path';
+import { Response } from 'express';
+
 
 @Controller('videos')
 export class VideosController {
@@ -9,5 +13,40 @@ export class VideosController {
     @Get()
     async sendRecently() {
         return this.videos.sendRecently();
+    }
+
+    @Header('Accept-Ranges', 'bytes')
+    @Header('Content-Type', 'video/mp4')
+    @Get('get')
+    async sendVideo(@Headers() headers, @Res() res: Response) {
+        const video = "/video-server/videos/kek.mp4";
+
+        const { size } = statSync(video);
+        const videoRange = headers.range;
+        if (videoRange) {
+            const parts = videoRange.replace(/bytes=/, '').split('-');
+
+            const start = parseInt(parts[0], 10);
+            const end = parts[1] ? parseInt(parts[1], 10) : size - 1;
+
+            const chunkSize = (end - start) + 1;
+
+            const readStreamFile = createReadStream(video, { start, end, highWaterMark: 60 });
+
+            const head = {
+                'Content-Range': `bytes ${start}-${end}/${size}`,
+                'Content-Length': chunkSize,
+            };
+            res.writeHead(HttpStatus.PARTIAL_CONTENT, head); //206
+            readStreamFile.pipe(res);
+        } else {
+            const head = {
+                'Content-Length': size,
+            };
+            res.writeHead(HttpStatus.OK, head);
+            createReadStream(video).pipe(res);
+
+        }
+
     }
 }
